4aa921f25f0a3f2d507768dbee8ac763988daa23
Pietro calebe <pietrorepositorio@gmail.com>
2025-07-13 17:21:00 -0300

    aprendedo  condiçoes na programação

---
+++ exercicios/aula12/exr011.js
@@ -0,0 +1,10 @@
+var idade  = 20 ;
+if (idade < 18){
+    console.log("nao pode votar");
+}
+else  if (idade >= 16 && idade < 18){{
+       console.log("pode votar, mas não pode dirigir");
+   }
+}else if (idade >= 18){
+    console.log("pode votar e dirigir");
+}
