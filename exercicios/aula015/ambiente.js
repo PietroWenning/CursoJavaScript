eef6d902b2a1ad1e58ca596c2fe53c649e7b6de8
Pietro calebe <pietrorepositorio@gmail.com>
2025-07-21 01:13:23 -0300

    variaveis compostas

--- exercicios/aula015/ambiente.js
+++ exercicios/aula015/ambiente.js
@@ -1,5 +1,13 @@
 let num = [5,2,8,9,3]
+num.push(1)
 num.sort()
 console.log(num)
 console.log(`o vetor tem ${num.length} posiçoes`)
 console.log(`o primeiro valor é ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log('O valor na foi encontrado!')
+}
+else{
+console.log(`o valor esta  na posicao ${pos}`)
+}
